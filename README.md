6db4678038a69142f01b07cd1dc0961646485c2b
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2021-03-04 00:13:42 -0300

    Now using gm_luaerror instead of EPOE
    - The script will now require servers to install the gm_luaerror module instead of EPOE to work.
    - Completely changed the structure of how errors are saved inside the error_logs folder. Errors will now be separated by the addon that caused it and the realm in which it ocurred.
    - Files will now contain a far more readable list of errors and the amount of times each of them has been detected.
    - Updated README.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Simple GLua Error Logger
 
-Adds a simple script to capture and save Lua errors on your server in a text file located inside `data/error_logs/`.
+Adds a simple script to capture and save Lua errors from your server.
 
-Requires [EPOE](https://github.com/Metastruct/EPOE) to work, otherwise it'll simply do nothing.
+**Requires [gm_luaerror](https://github.com/danielga/gm_luaerror) module to work. The script will simply error if this module is missing. Only the server needs to have the module installed.**
+
+You can find all the captured errors inside the `garrysmod/data/error_logs/` folder. The structure used to save these logs will be `error_logs/YYYY-MM-DD/addon-name/realm-name.txt` for easier addon error debugging. Inside of these files, you'll find a list of errors and the amount of times they've been detected during the day.
--- lua/autorun/server/error_logger.lua
+++ lua/autorun/server/error_logger.lua
@@ -1,24 +1,122 @@
-local file    = file
-local os      = os
-local string  = string
-local Pattern = ".lua:%d+:" -- Will store messages that point out a Lua file
-local Message = "[%s]\n%s\n\n"
-local Folder  = "error_logs"
-local Path    = Folder .. "/%s.txt"
+require("luaerror")
 
-hook.Add("EngineSpew", "Error Logger", function(_, Error)
-	if not string.match(Error, Pattern) then return end
+-- Enabling everything
+luaerror.EnableRuntimeDetour(true)
+luaerror.EnableCompiletimeDetour(true)
+luaerror.EnableClientDetour(true)
 
-	local File    = Path:format(os.date("%Y-%m-%d"))
-	local Content = Message:format(os.date("%X"), Error:Trim())
+local file        = file
+local os          = os
+local string      = string
+local ErrorLog    = {}
+local FilePattern = "^addons/(.-)/"
+local FolderPath  = "error_logs/%s/%s"
+local FilePath    = FolderPath .. "/%s.txt"
+local ErrorText   = "Times detected: %s time(s)\n%s\n\n"
+local Identifier  = "Simple GLua Error Logger"
+local CurrentDate, QueueSave
 
-	if file.Exists(File, "DATA") then
-		file.Append(File, Content)
-	else
-		if not file.Exists(Folder, "DATA") then
-			file.CreateDir(Folder)
+-- Recursively gets or creates tables based on the provided arguments.
+local function GetTable(...)
+	local Result = ErrorLog
+	local Keys   = { ... }
+
+	for _, Key in ipairs(Keys) do
+		local Value = Result[Key]
+
+		if not Value then
+			Value = {}
+
+			Result[Key] = Value
 		end
 
-		file.Write(File, Content)
+		Result = Value
 	end
+
+	return Result
+end
+
+-- Turns a table of errors into a neat list string.
+local function GetErrorList(Errors)
+	local List = ""
+
+	for Error, Count in pairs(Errors) do
+		List = List .. ErrorText:format(Count, Error)
+	end
+
+	return List
+end
+
+-- Saves the list of errors into a text file.
+local function SaveContents(Date, Addon, Realm, Contents)
+	local Folder = FolderPath:format(Date, Addon)
+	local Path   = FilePath:format(Date, Addon, Realm)
+
+	if not file.Exists(Folder, "DATA") then
+		file.CreateDir(Folder)
+	end
+
+	file.Write(Path, Contents)
+end
+
+-- Saves all errors from the error log and cleans it if the day has changed
+local function SaveErrors()
+	for Addon, Realms in pairs(ErrorLog) do
+		for Realm, Errors in pairs(Realms) do
+			local Contents = GetErrorList(Errors)
+
+			SaveContents(CurrentDate, Addon, Realm, Contents)
+		end
+	end
+
+	local Date = os.date("%Y-%m-%d")
+
+	if Date ~= CurrentDate then
+		-- New day, new errors
+		for K in pairs(ErrorLog) do
+			ErrorLog[K] = nil
+		end
+
+		CurrentDate = Date
+	end
+
+	QueueSave = nil
+end
+
+-- Stores an error into the error log table.
+local function SaveError(File, Realm, Error)
+	local Addon
+
+	if File then
+		Addon = string.match(File, FilePattern) or File
+	else
+		Addon = "unknown"
+	end
+
+	local Table = GetTable(Addon, Realm)
+	local Count = Table[Error] or 0
+
+	Table[Error] = Count + 1
+
+	if not QueueSave then
+		timer.Simple(1, SaveErrors)
+
+		QueueSave = true
+	end
+end
+
+hook.Add("Initialize", Identifier, function()
+	CurrentDate = os.date("%Y-%m-%d")
+
+	hook.Remove("Initialize", Identifier)
 end)
+
+hook.Add("LuaError", Identifier, function(_, Error, File)
+	SaveError(File, "server", Error)
+end)
+
+hook.Add("ClientLuaError", Identifier, function(_, Error, File)
+	SaveError(File, "client", Error)
+end)
+
+hook.Add("Shutdown", Identifier, SaveErrors)
